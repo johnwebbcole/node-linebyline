a8d89c2c23c548009aa8587bda15432a786fdfd4
John Cole <johnwebbcole@gmail.com>
2014-03-05 16:18:07 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Node-LineByLine
+===============
+
+Node stream that transforms a text stream into a line by line stream.  Has an option to ignore the first line if the text file has  header line.
+
+
---
+++ linebyline.js
@@ -0,0 +1,49 @@
+module.exports = LineByLine;
+
+var Transform = require('stream').Transform;
+var util = require('util');
+util.inherits(LineByLine, Transform);
+
+function LineByLine(options) {
+	if (!(this instanceof LineByLine)) return new LineByLine(options);
+	this.buffer = '';
+	this.hasheader = options.hasheader || true;
+	this.line_number = 0;
+	Transform.call(this, options);
+}
+
+
+LineByLine.prototype._transform = function(data, encoding, cb) {
+	if (!this.encoding) {
+		this.encoding = encoding || 'utf8';
+	}
+
+	if (Buffer.isBuffer(data)) {
+		data = data.toString(encoding || 'utf8');
+	}
+
+	var parts = data.split(/\n|\r\n/g);
+
+	if (this.buffer.length > 0) {
+		parts[0] = this.buffer + parts[0];
+	}
+
+	for (var i = 0; i < parts.length - 1; i++) {
+		if (!(this.hasheader && this.line_number === 0)) {
+			this.push(parts[i], this.encoding);
+		}
+		this.line_number++;
+	}
+
+	this.buffer = parts[parts.length - 1];
+
+	cb(null);
+};
+
+LineByLine.prototype._flush = function(cb) {
+	if (this.buffer.lenght > 0) {
+		this.push(buffer, this.encoding);
+		this.line_number++;
+	}
+	cb();
+};
